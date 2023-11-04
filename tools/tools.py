0161ab534c2c5b3722fb33c2ed122ab1bf2eab32
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-11-04 15:17:44 +0800

    使用cython优化

--- tools/tools.py
+++ tools/tools.py
@@ -1,17 +1,22 @@
 import argparse
-from subprogram import updateLog, parameterFree, proxy
+from lib import updateLog as ul
+from lib import parameterFree as pf
+from subprogram import proxy
 
 # 创建命令行解析器
-parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser = argparse.ArgumentParser()
 
 # 添加命令行参数
 parser.add_argument('command', nargs='?')
+# parser.add_argument("update-log", type=str, help="显示更新日志")
+# parser.add_argument("proxy", type=str, help="修改启动代理")
 
 # 定义自定义帮助信息
-custom_help = """
-可用命令:
-update-log       显示更新日志
-"""
+# custom_help = """
+# 可用命令:
+# update-log       显示更新日志
+# proxy            修改启动代理
+# """
 
 # 解析命令行参数
 args = parser.parse_args()
@@ -20,13 +25,14 @@ args = parser.parse_args()
 if args.command:
     # 根据解析结果执行相应操作
     if args.command == 'update-log':
-        # 执行updateLog.update_log()的代码
-        updateLog.update_log()
+        ul.update_log()
     if args.command == 'proxy':
         proxy.site()
     else:
         print('未知命令:', args.command)
-        parameterFree.parameter_free_unknow()
+        pf.parameter_free_unknow()
 else:
     # 执行无参数操作的代码
-    parameterFree.parameter_free()
+    # pf.parameter_free()
+    # 如果没有提供任何参数，则打印帮助信息
+    parser.print_help()
