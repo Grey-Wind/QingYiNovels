cbbf443eaabe07533d9bc269bb46ca1268309b24
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-11-05 00:30:27 +0800

    优化代码

--- tools/tools.py
+++ tools/tools.py
@@ -1,43 +1,36 @@
 import argparse
 from lib import updateLog as ul
-from lib import parameterFree as pf
 from lib import package
 from subprogram import proxy
 
 # 创建命令行解析器
 parser = argparse.ArgumentParser()
 
-# 添加命令行参数
-parser.add_argument('command', nargs='?')
-# parser.add_argument("update-log", type=str, help="显示更新日志")
-# parser.add_argument("proxy", type=str, help="修改启动代理")
+# 创建一个互斥的参数组
+group = parser.add_mutually_exclusive_group(required=True)
 
-# 定义自定义帮助信息
-# custom_help = """
-# 可用命令:
-# update-log       显示更新日志
-# proxy            修改启动代理
-# """
+# 添加命令行参数
+group.add_argument("update_log", nargs='?', help="显示更新日志")
+group.add_argument("proxy", nargs='?', help="修改启动代理")
+group.add_argument("package-install", nargs='?', help="安装离线包")
+group.add_argument("package-uninstall", nargs='?', help="卸载离线包")
 
 # 解析命令行参数
 args = parser.parse_args()
 
-# 检查是否存在命令参数
-if args.command:
-    # 根据解析结果执行相应操作
-    if args.command == 'update-log':
-        ul.update_log()
-    if args.command == 'proxy':
-        proxy.site()
-    if args.command == 'package-install':
-        package.install()
-    if args.command == 'package-uninstall':
-        package.uninstall()
-    else:
-        print('未知命令:', args.command)
-        pf.parameter_free_unknow()
+# 根据解析结果执行相应操作
+if args.update_log:
+    ul.update_log()
+elif args.proxy:
+    proxy.site()
+elif args.package_install:
+    package.install()
+elif args.package_uninstall:
+    package.uninstall()
 else:
+    print('未知命令')
     # 执行无参数操作的代码
     # pf.parameter_free()
+    # pf.parameter_free_unknow()
     # 如果没有提供任何参数，则打印帮助信息
     parser.print_help()
