f12df57107e429adb14d7006e39cf1dcaa3d9b73
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-11-05 21:19:23 +0800

    优化cython构建方式

--- tools/subprogram/cython_build.bat
+++ tools/subprogram/cython_build.bat
@@ -1,2 +1,19 @@
+@echo off
+
+rmdir /s /q build
+rmdir /s /q __pycache__
+
+setlocal enabledelayedexpansion
+
+set "folderPath=./"
+set "extensions=.c;.pyd;.pyc"
+
+for %%E in (%extensions%) do (
+    for /r "%folderPath%" %%F in (*%%E) do (
+        del "%%F"
+        echo Deleted: %%F
+    )
+)
+
 python setup.py build_ext --inplace
 pause
